d15baba525286079c8ca515370abe80a4b5a2c92
ProgVitor <vhferprog@gmail.com>
2019-05-28 23:53:52 +0100

    icons novos

--- projpa/Iu/Graphic/Panes/ActionPane.java
+++ projpa/Iu/Graphic/Panes/ActionPane.java
@@ -10,8 +10,6 @@ import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -27,8 +25,7 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import javafx.scene.shape.Circle;
 import projpa.GameLogic.Alien;
 
@@ -66,6 +63,11 @@ public class ActionPane extends StackPane implements Constants, PropertyChangeLi
     private VBox rightBox;
     private final int maxnumberofshownalien = 20;
     private ArrayList<Label> alienslabel;
+    ArrayList<String> menu;
+    Label healthTracker;
+    Label HullTracker;
+    ImageView healthIcon;
+    ImageView hullIcon;
 
     public ActionPane(GameLogic game) {
         this.game = game;
@@ -109,7 +111,7 @@ public class ActionPane extends StackPane implements Constants, PropertyChangeLi
         tabLayout.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
 
         firstTabImage = new VBox();
-        firstTabLabel = new Label("Doctor");
+        firstTabLabel = new Label("Crew 1");
 
         firstTabLabel.setTextFill(Color.web("#ffffff"));
 
@@ -124,7 +126,7 @@ public class ActionPane extends StackPane implements Constants, PropertyChangeLi
         tabLayout.getTabs().add(firstTab);
 
         secondTabImage = new VBox();
-        secondTabLabel = new Label("Red Shirt");
+        secondTabLabel = new Label("Crew 2");
 
         secondTabLabel.setTextFill(Color.web("#ffffff"));
 
@@ -138,8 +140,11 @@ public class ActionPane extends StackPane implements Constants, PropertyChangeLi
         secondTab.setGraphic(secondTabImage);
         tabLayout.getTabs().add(secondTab);
 
+        tabLayout.getSelectionModel().select(0);
+
         leftBox = new VBox(tabLayout);
         leftBox.getStyleClass().add("rightBox");
+
 
         leftBox.setAlignment(Pos.TOP_CENTER);
 
@@ -168,12 +173,25 @@ public class ActionPane extends StackPane implements Constants, PropertyChangeLi
         ipIcon.setFitWidth(20);
         ipIcon.setFitHeight(20);
         lblIp = new Label(": " + this.game.getInspirationPoints(), ipIcon);
-
         lblIp.setTextFill(Color.web("#ffffff"));
         lblIp.getStyleClass().add("PointsLabel");
 
+        healthIcon = new ImageView(new Image(getClass().getResourceAsStream("..\\Images\\health.png")));
+        healthIcon.setFitWidth(20);
+        healthIcon.setFitHeight(20);
+        healthTracker = new Label(": " + this.game.getHealthTrackerHealth(), healthIcon);
+        healthTracker.setTextFill(Color.web("#ffffff"));
+        healthTracker.getStyleClass().add("PointsLabel");
+
+        hullIcon = new ImageView(new Image(getClass().getResourceAsStream("..\\Images\\hull.png")));
+        hullIcon.setFitWidth(20);
+        hullIcon.setFitHeight(20);
+        HullTracker = new Label(": " + this.game.getHullState(), hullIcon);
+        HullTracker.setTextFill(Color.web("#ffffff"));
+        HullTracker.getStyleClass().add("PointsLabel");
+
         topRightBox = new HBox();
-        topRightBox.getChildren().addAll(lblPoints, lblAp, lblIp);
+        topRightBox.getChildren().addAll(HullTracker, healthTracker, lblPoints, lblAp, lblIp);
         topRightBox.setAlignment(Pos.CENTER_RIGHT);
         topRightBox.setSpacing(10);
 
@@ -220,36 +238,26 @@ public class ActionPane extends StackPane implements Constants, PropertyChangeLi
         rightBox.setPadding(new Insets(5, 10, 5, 10));
         
 
-        ArrayList<String> menu = new ArrayList<String>();
+        menu = new ArrayList<String>();
         alienslabel = new ArrayList<Label>();
-        //menu = updaterightbox();        
-              
-        Label lblPoint = new Label(": xxx", pointsIcon);
-        Label titulealien = new Label("List Of Aliens:");
-        titulealien.setTextFill(Color.web("#ffffff"));
-        titulealien.getStyleClass().add("AliensLabel");
-        
         
         for (int i = 0; i< maxnumberofshownalien; i++){ // TEMP -> REVERRR
             
             if(i==0){
                 //alienslabel.add(new Label("List Of Aliens:                          "));
-                alienslabel.add(new Label(String.format("%-60s", "List Of Aliens: ")));
-                
-                alienslabel.get(i).setTextFill(Color.web("#ffffff"));
-                alienslabel.get(i).getStyleClass().add("AliensLabel");
+                alienslabel.add(new DeathStarLabel("List Of Aliens: ", 20));
                 alienslabel.get(i).setVisible(true);
             }
             else{
-                alienslabel.add(new Label("Alien1: "));
+                alienslabel.add(new Label("Alien1:  "));
                 alienslabel.get(i).setTextFill(Color.web("#ffffff"));
                 alienslabel.get(i).getStyleClass().add("AliensLabel");
                 alienslabel.get(i).setVisible(false);
             }
         }
         
-        
         rightBox.getChildren().addAll(alienslabel);
+        rightBox.setSpacing(20);
         rightBox.getStyleClass().add("rightBox");
         rightBox.setAlignment(Pos.TOP_LEFT);
 
@@ -448,15 +456,80 @@ public class ActionPane extends StackPane implements Constants, PropertyChangeLi
         }
     }
 
+    private void refreshLeftPanel(){
+
+        DeathStarLabel Crew1Name = new DeathStarLabel(this.game.getCrewmember1Name(), 20);
+        Crew1Name.setPadding(new Insets(20, 0, 0,0));
+        DeathStarLabel Crew2Name = new DeathStarLabel(this.game.getCrewmember2Name(), 20);
+        Crew2Name.setPadding(new Insets(20, 0, 0,0));
+
+        DeathStarLabel LCrew1Local = new DeathStarLabel("Room:  " + this.game.getCrewmember1RoomName(), 15);
+        DeathStarLabel LCrew2Local = new DeathStarLabel("Room:  " + this.game.getCrewmember2RoomName(), 15);
+
+        DeathStarLabel ACrew1Local = new DeathStarLabel("Atack:  " + this.game.getCrewMember1Atack(), 15);
+        DeathStarLabel ACrew2Local = new DeathStarLabel("Atack:  " + this.game.getCrewMember2Atack(), 15);
+
+        DeathStarLabel MCrew1Local = new DeathStarLabel("Movement:  " + this.game.getCrewMember1Move(), 15);
+        DeathStarLabel MCrew2Local = new DeathStarLabel("Movement:  " + this.game.getCrewMember2Move(), 15);
+
+        DefaultButton btnMoveCrew1 = new DefaultButton("Move", new Image(getClass().getResourceAsStream("..\\Images\\move.png")), 20 , 20, 120, 40);
+        DefaultButton btnAtackCrew1 = new DefaultButton("Atack", new Image(getClass().getResourceAsStream("..\\Images\\sword.png")), 20 , 20, 120, 40);
+        DefaultButton btnHealCrew1 = new DefaultButton("Heal", new Image(getClass().getResourceAsStream("..\\Images\\potion.png")), 20 , 20, 120, 40);
+        DefaultButton btnSacrificeCrew1 = new DefaultButton("Sacrifice", new Image(getClass().getResourceAsStream("..\\Images\\death.png")), 20 , 20, 120, 40);
+
+        VBox firstCrewVBox = new VBox();
+        firstCrewVBox.getChildren().addAll(btnMoveCrew1, btnAtackCrew1, btnHealCrew1, btnSacrificeCrew1);
+        firstCrewVBox.setAlignment(Pos.CENTER);
+        firstCrewVBox.setSpacing(20);
+
+        DefaultButton btnMoveCrew2 = new DefaultButton("Move", new Image(getClass().getResourceAsStream("..\\Images\\move.png")), 20 , 20, 120, 40);
+        DefaultButton btnAtackCrew2 = new DefaultButton("Atack", new Image(getClass().getResourceAsStream("..\\Images\\sword.png")), 20 , 20, 120, 40);
+        DefaultButton btnHealCrew2 = new DefaultButton("Heal", new Image(getClass().getResourceAsStream("..\\Images\\potion.png")), 20 , 20, 120, 40);
+        DefaultButton btnSacrificeCrew2 = new DefaultButton("Sacrifice", new Image(getClass().getResourceAsStream("..\\Images\\death.png")), 20 , 20, 120, 40);
+
+        if (!this.game.getCrewmember1Name().equals("Doctor"))
+            btnHealCrew1.setDisable(true);
+        if (!this.game.getCrewmember2Name().equals("Doctor"))
+            btnHealCrew2.setDisable(true);
+
+        if (!this.game.getCrewmember1Name().equals("Red Shirt"))
+            btnSacrificeCrew1.setDisable(true);
+        if (!this.game.getCrewmember2Name().equals("Red Shirt"))
+            btnSacrificeCrew2.setDisable(true);
+
+        VBox secondCrewVBox = new VBox();
+        secondCrewVBox.getChildren().addAll(btnMoveCrew2, btnAtackCrew2, btnHealCrew2, btnSacrificeCrew2);
+        secondCrewVBox.setAlignment(Pos.CENTER);
+        secondCrewVBox.setSpacing(20);
+
+        VBox firstTabVbox = new VBox();
+        firstTabVbox.getChildren().addAll(Crew1Name, LCrew1Local, ACrew1Local, MCrew1Local, firstCrewVBox);
+        firstTabVbox.setAlignment(Pos.TOP_CENTER);
+        firstTabVbox.setPadding(new Insets(0,0,0,5));
+        firstTabVbox.setSpacing(20);
+
+        VBox secondTabVbox = new VBox();
+        secondTabVbox.getChildren().addAll(Crew2Name, LCrew2Local, ACrew2Local, MCrew2Local, secondCrewVBox);
+        secondTabVbox.setAlignment(Pos.TOP_CENTER);
+        secondTabVbox.setSpacing(20);
+        secondTabVbox.setPadding(new Insets(0,0,0,5));
+
+        secondTab.setContent(secondTabVbox);
+        firstTab.setContent(firstTabVbox);
+
+    }
+
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
 
         if (this.game.inAwaitCrewPhaseActions()){
             refreshJourneyTracker();
+            refreshLeftPanel();
         }
 
         setVisible(this.game.inAwaitCrewPhaseActions());
         updaterightbox();
+
     }
 
     private void updaterightbox() {
@@ -469,7 +542,7 @@ public class ActionPane extends StackPane implements Constants, PropertyChangeLi
             if(i < maxnumberofshownalien - 1)
             
             {
-            alienslabel.get(i+1).setText("Alien " + (i+1) + " in: " + returnalien.get(i).getRoom().getName());
+            alienslabel.get(i+1).setText("Alien  " + (i+1) + "  in:  " + returnalien.get(i).getRoom().getName());
             alienslabel.get(i+1).setVisible(true);
             
             }
